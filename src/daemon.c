618ff572648f5669d9908183f26fe9b0595e66be
jonz <jonz>
2004-11-21 02:47:55 +0000

    added client/server skeleton

---
+++ src/daemon.c
@@ -0,0 +1,49 @@
+/* $Id: daemon.c,v 1.1 2004/11/21 02:47:55 jonz Exp $ */
+
+/*
+ DSPAM
+ COPYRIGHT (C) 2002-2004 NETWORK DWEEBS CORPORATION
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+
+#ifdef DAEMON
+
+#ifdef HAVE_CONFIG_H
+#include <auto-config.h>
+#endif
+
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <errno.h>
+#include <error.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#ifndef _WIN32
+#include <unistd.h>
+#include <pwd.h>
+#endif
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "libdspam.h"
+#include "config.h"
+#include "util.h"
+#include "language.h"
+
+#endif /* DAEMON */
+
---
+++ src/daemon.h
@@ -0,0 +1,39 @@
+/* $Id: daemon.h,v 1.1 2004/11/21 02:47:55 jonz Exp $ */
+
+/*
+ DSPAM
+ COPYRIGHT (C) 2002-2004 NETWORK DWEEBS CORPORATION
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+
+#ifdef DAEMON
+
+#ifndef _DAEMON_H
+#  define _DAEMON_H
+
+#include <sys/types.h>
+#ifndef _WIN32
+#include <pwd.h>
+#endif
+
+#ifdef HAVE_CONFIG_H
+#include <auto-config.h>
+#endif
+
+#endif /* _DAEMON_H */
+
+#endif
