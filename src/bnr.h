407c7a15d58cd18805acbc635f23d85fe45f5964
jonz <jonz>
2005-04-21 02:15:06 +0000

    recommenting

--- src/bnr.h
+++ src/bnr.h
@@ -1,23 +1,23 @@
-/* $Id */
+/* $Id: bnr.h,v 1.7 2005/04/21 02:15:06 jonz Exp $ */
 
 /*
-  Bayesian Noise Reduction - Contextual Symmetry Logic
-  http://bnr.nuclearelephant.com
-  Copyright (c) 2004 Jonathan A. Zdziarski
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
+ DSPAM
+ COPYRIGHT (C) 2002-2005 DEEP LOGIC INC.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
 
