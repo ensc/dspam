f9fcf49064280043731eda74e14656bf50fb7431
jonz <jonz>
2005-04-22 21:03:18 +0000

    updated

--- src/example.c
+++ src/example.c
@@ -1,31 +1,35 @@
-/* $Id: example.c,v 1.3 2005/04/05 22:17:51 jonz Exp $ */
+/* $Id: example.c,v 1.4 2005/04/22 21:03:18 jonz Exp $ */
 
 /*
  DSPAM
- COPYRIGHT (C) 2002-2004 NETWORK DWEEBS CORPORATION
+ COPYRIGHT (C) 2002-2005 DEEP LOGIC INC.
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-                                                                                
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
 */
 
 /*
-   examples of libdspam implementation
-   compile with:
-   gcc -o example example.c -ldspam -L.libs -I. -DHAVE_CONFIG_H \
-     -DCONFIG_DEFAULT=/etc/dspam.conf
-*/
+ * example.c - example of libdspam implementation
+ *
+ * DESCRIPTION
+ *
+ * 
+ * compile with:
+ * gcc -o example example.c -ldspam -L.libs -I. -DHAVE_CONFIG_H \
+ *   -DCONFIG_DEFAULT=/usr/local/etc/dspam.conf
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
