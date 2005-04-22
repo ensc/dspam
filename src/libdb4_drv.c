71832b081f4dc0f09329472682d45562903963ce
jonz <jonz>
2005-04-22 21:11:35 +0000

    recommented (not sure if i want to bother auditing these crummy drivers)

--- src/libdb4_drv.c
+++ src/libdb4_drv.c
@@ -1,24 +1,32 @@
-/* $Id: libdb4_drv.c,v 1.8 2005/04/22 20:26:44 jonz Exp $ */
+/* $Id: libdb4_drv.c,v 1.9 2005/04/22 21:11:35 jonz Exp $ */
 
-/*
+*
  DSPAM
- COPYRIGHT (C) 2002-2004 NETWORK DWEEBS CORPORATION
+ COPYRIGHT (C) 2002-2005 DEEP LOGIC INC.
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
+
+/*
+ * libdb4_drv.c - Berkeley DB v3 storage driver *
+ * DESCRIPTION
+ *   Do not use this driver. BDB is terrible, and will likely screw you
+ *   over royally at some point. Please consider using the SQLite driver
+ *   before attempting to use BDB.
+ */
 
 #ifdef HAVE_CONFIG_H
 #include <auto-config.h>
