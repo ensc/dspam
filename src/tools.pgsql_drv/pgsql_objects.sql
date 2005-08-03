56c8f4fbf2a5cb89e13f1cba1e1114a57d49873a
jonz <jonz>
2005-08-03 12:37:47 +0000

    added lookup_tokens for 8.0+

--- src/tools.pgsql_drv/pgsql_objects.sql
+++ src/tools.pgsql_drv/pgsql_objects.sql
@@ -1,4 +1,4 @@
-/* $Id: pgsql_objects.sql,v 1.8 2005/07/08 02:17:54 jonz Exp $ */
+/* $Id: pgsql_objects.sql,v 1.9 2005/08/03 12:37:47 jonz Exp $ */
 
 CREATE TABLE dspam_token_data (
   uid smallint,
@@ -55,3 +55,22 @@ CREATE TABLE dspam_preferences (
   value varchar(128),
   UNIQUE (uid, preference)
 ) WITHOUT OIDS;
+
+create function lookup_tokens(integer,bigint[])
+  returns setof dspam_token_data
+  language plpgsql stable
+  as '
+declare
+  v_rec record;
+begin
+  for v_rec in select * from dspam_token_data
+                where uid=$1
+                  and token in (select $2[i]
+                                  from generate_series(array_lower($2,1),
+                                                       array_upper($2,1)) s(i))
+  loop
+    return next v_rec;
+  end loop;
+  return;
+end;';
+
