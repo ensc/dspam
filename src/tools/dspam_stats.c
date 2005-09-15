deb486d3239a52e40460a5bf0b6305e244a4bbde
jonz <jonz>
2005-09-15 02:19:07 +0000

    rc1

--- src/tools/dspam_stats.c
+++ src/tools/dspam_stats.c
@@ -1,4 +1,4 @@
-/* $Id: dspam_stats.c,v 1.15 2005/09/10 18:27:47 jonz Exp $ */
+/* $Id: dspam_stats.c,v 1.16 2005/09/15 02:19:07 jonz Exp $ */
 
 /*
  DSPAM
@@ -328,9 +328,9 @@ stat_user (const char *username)
   else
   {
 #ifdef LONG_USERNAMES
-    printf ("%s\n    TS:%6ld TN:%6ld FN:%6ld FP:%6ld SC:%6ld IC:%6ld\n",
+    printf ("%s\n    TS:%6ld TI:%6ld FN:%6ld FP:%6ld SC:%6ld IC:%6ld\n",
 #else
-    printf ("%-16s  TS:%6ld TN:%6ld FN:%6ld FP:%6ld SC:%6ld IC:%6ld\n",
+    printf ("%-16s  TS:%6ld TI:%6ld FN:%6ld FP:%6ld SC:%6ld IC:%6ld\n",
 #endif
             username,
             total_spam, total_innocent,
