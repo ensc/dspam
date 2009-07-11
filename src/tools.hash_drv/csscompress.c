a64bcf628e999b766f54e210eed9ab4569a4c768
Stevan Bajic <stevan@bajic.ch>
2009-07-11 16:56:32 +0200

    Fixing bug in cssconvert and csscompress (closing SF Bug ID #2819960)  - Thanks Julien Valroff <valroff@users.sourceforge.net> for the note    about the bugfix in Debian.
    MFM: 21 days
    Bug-ID: 2819960
    Bug-Reported-By: Dean Takemori <deantakemori@users.sourceforge.net>
    Security: none

--- src/tools.hash_drv/csscompress.c
+++ src/tools.hash_drv/csscompress.c
@@ -1,4 +1,4 @@
-/* $Id: csscompress.c,v 1.8 2006/05/27 21:00:36 jonz Exp $ */
+/* $Id: csscompress.c,v 1.81 2009/07/11 16:52:42 sbajic Exp $ */
 
 /*
  DSPAM
@@ -134,7 +134,7 @@ int csscompress(const char *filename) {
     }
   }
 
-  snprintf(newfile, sizeof(newfile), "/tmp/%u.css", (unsigned int) getpid());
+  snprintf(newfile, sizeof(newfile), "/%s/.dspam%u.css", dirname(filename), (unsigned int) getpid());
 
   if (_hash_drv_open(filename, &old, 0, max_seek,
                      max_extents, extent_size, pctincrease, flags))
