79d66d32c7cfe506bff17fac4c296d25eae13e84
Stevan Bajic <stevan@bajic.ch>
2009-12-18 01:02:53 +0100

    Updated purge.sql SQL script for MySQL
    MFM: 0 days
    Bug-ID: none
    Bug-Reported-By: none
    Security: none

--- src/tools.mysql_drv/purge.sql
+++ src/tools.mysql_drv/purge.sql
@@ -1,14 +1,37 @@
-# $Id: purge.sql,v 1.6 2009/05/25 11:46:46 mjohnson Exp $
-set @a=to_days(current_date());
-delete from dspam_token_data 
-  where (innocent_hits*2) + spam_hits < 5
-  and from_days(@a-60) > last_hit;
-delete from dspam_token_data
-  where ((innocent_hits = 1 and spam_hits = 0) or (innocent_hits = 0 and spam_hits = 1))
-  and from_days(@a-15) > last_hit;
-delete from dspam_token_data
-  where from_days(@a-90) > last_hit;
-delete from dspam_signature_data
-  where from_days(@a-14) > created_on;
+-- $Id: purge.sql,v 1.7 2009/12/18 00:43:54 sbajic Exp $
 
-optimize table dspam_token_data, dspam_signature_data;
+--
+-- Set some defaults
+--
+SET @today=to_days(current_date());
+SET @stale=15;
+
+--
+-- Delete fairly neutral tokens older than 4*@stale days
+--
+DELETE
+  FROM dspam_token_data
+    WHERE from_days(@today-(4*@stale)) > last_hit
+      AND (innocent_hits*2) + spam_hits < 5;
+
+--
+-- Delete tokens seen only one time per class in the past @stale days
+--
+DELETE
+  FROM dspam_token_data
+    WHERE from_days(@today-@stale) > last_hit
+      AND ((innocent_hits = 1 AND spam_hits = 0) OR (innocent_hits = 0 AND spam_hits = 1));
+
+--
+-- Delete all tokens never seen in the last 4*@stale days
+--
+DELETE
+  FROM dspam_token_data
+    WHERE from_days(@today-(6*@stale)) > last_hit;
+
+--
+-- Delete signatures older than @stale days
+--
+DELETE
+  FROM dspam_signature_data
+    WHERE from_days(@today-(@stale-1)) > created_on;
