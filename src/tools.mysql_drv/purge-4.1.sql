460d5d1d67bb2775c49f4b62071d934c696c0a2a
Stevan Bajic <stevan@bajic.ch>
2009-12-19 16:51:00 +0100

    Updated purge-4.1.sql SQL script for MySQL (Nicolas Grekas did a great job in simplifying/enhancing the original file)
    MFM: 0 days
    Bug-ID: none
    Bug-Reported-By: Nicolas Grekas <nicolas.grekas@espci.org>
    Security: none

--- src/tools.mysql_drv/purge-4.1.sql
+++ src/tools.mysql_drv/purge-4.1.sql
@@ -1,96 +1,87 @@
--- $Id: purge-4.1.sql,v 1.8 2008/12/18 01:44:36 sbajic Exp $
+-- $Id: purge-4.1.sql,v 1.9 2008/12/19 16:32:16 sbajic Exp $
+
+-- ---------------------------------------------------------------------------
+-- Note: Should you have modified your dspam.conf to have other intervals for
+--       the purging or you have modified the TrainingMode to be other then
+--       'TEFT' then please modify this SQL file to be in sync with your
+--       dspam.conf.
+--
+-- Note: It is difficult to purge unused tokens with SQL clauses the same way
+--       as dspam_clean is doing it. So you should still run dspam_clean with
+--       the "-u" parameter from time to time.
+-- ---------------------------------------------------------------------------
 
 --
 -- Set some defaults
 --
-SET @stale=15;
-SET @today=to_days(current_date());
-SET @trainingmode=(
-  SELECT COALESCE(
-    (SELECT value
-      FROM dspam_preferences
-        WHERE uid = 0
-          AND preference = 'trainingMode'
-        LIMIT 1
-    )
-  ,'TEFT')
-);
+SET @TrainingMode    = 'TEFT';      -- Default training mode
+SET @PurgeSignatures = 14;          -- Stale signatures
+SET @PurgeUnused     = 90;          -- Unused tokens
+SET @PurgeHapaxes    = 30;          -- Tokens with less than 5 hits (hapaxes)
+SET @PurgeHits1S     = 15;          -- Tokens with only 1 spam hit
+SET @PurgeHits1I     = 15;          -- Tokens with only 1 innocent hit
+SET @today           = to_days(current_date());
 
 --
--- Delete fairly neutral tokens older than 4*@stale days
+-- Delete tokens with less than 5 hits (hapaxes)
 --
 START TRANSACTION;
 DELETE LOW_PRIORITY QUICK
   FROM dspam_token_data
-    WHERE from_days(@today-(4*@stale)) > last_hit
-      AND (innocent_hits*2) + spam_hits < 5;
+    WHERE from_days(@today-@PurgeHapaxes) > last_hit
+      AND (2*innocent_hits)+spam_hits < 5;
 COMMIT;
 
 --
--- Delete tokens seen only one time per class in the past @stale days
+-- Delete tokens with only 1 spam hit
 --
 START TRANSACTION;
 DELETE LOW_PRIORITY QUICK
   FROM dspam_token_data
-    WHERE from_days(@today-@stale) > last_hit
-      AND ((innocent_hits = 1 AND spam_hits = 0) OR (innocent_hits = 0 AND spam_hits = 1));
+    WHERE from_days(@today-@PurgeHits1S) > last_hit
+      AND innocent_hits = 0 AND spam_hits = 1;
 COMMIT;
 
 --
--- Delete all tokens never seen in the last 4*@stale days, except for TOE, TUM and NOTRAIN modes
+-- Delete tokens with only 1 innocent hit
 --
+START TRANSACTION;
+DELETE LOW_PRIORITY QUICK
+  FROM dspam_token_data
+    WHERE from_days(@today-@PurgeHits1I) > last_hit
+      AND innocent_hits = 1 AND spam_hits = 0;
+COMMIT;
 
--- For users having set 'trainingMode' in their preferences
+--
+-- Delete unused tokens, except for TOE, TUM and NOTRAIN modes
+--
 START TRANSACTION;
 DELETE LOW_PRIORITY QUICK
   FROM t USING dspam_token_data t
-    LEFT JOIN dspam_preferences p
-      ON (t.uid = p.uid AND p.preference = 'trainingMode' AND p.value NOT IN ('TOE','TUM','NOTRAIN') LIMIT 1)
-  WHERE from_days(@today-(4*@stale)) > last_hit;
+    LEFT JOIN dspam_preferences p ON p.preference = 'trainingMode' AND p.uid = t.uid
+    LEFT JOIN dspam_preferences d ON d.preference = 'trainingMode' AND d.uid = 0
+  WHERE COALESCE(p.value,d.value,@TrainingMode) NOT IN ('TOE','TUM','NOTRAIN')
+    AND from_days(@today-@PurgeUnused) > last_hit;
 COMMIT;
 
--- For users NOT having set 'trainingMode' in their preferences (therefor using default/uid 0)
+--
+-- Delete TUM tokens seen no more than 50 times
+--
 START TRANSACTION;
 DELETE LOW_PRIORITY QUICK
   FROM t USING dspam_token_data t
-    LEFT JOIN dspam_preferences p
-      ON (t.uid = p.uid)
-  WHERE @trainingmode NOT IN ('TOE','TUM','NOTRAIN')
-    AND NOT EXISTS (SELECT uid FROM dspam_preferences WHERE uid = t.uid AND preference = 'trainingMode' LIMIT 1)
-    AND from_days(@today-(4*@stale)) > last_hit;
+    LEFT JOIN dspam_preferences p ON p.preference = 'trainingMode' AND p.uid = t.uid
+    LEFT JOIN dspam_preferences d ON d.preference = 'trainingMode' AND d.uid = 0
+  WHERE COALESCE(p.value,d.value,@TrainingMode) = 'TUM'
+    AND from_days(@today-@PurgeUnused) > last_hit
+    AND innocent_hits + spam_hits < 50;
 COMMIT;
 
 --
--- Delete TUM tokens seen no more than 50 times in any class for the last 6*@stale days
---
-
--- For users having set 'trainingMode' in their preferences
-START TRANSACTION;
-  DELETE LOW_PRIORITY QUICK
-    FROM t USING dspam_token_data t
-      LEFT JOIN dspam_preferences p
-        ON (t.uid = p.uid AND p.preference = 'trainingMode' AND p.value = 'TUM' LIMIT 1)
-    WHERE from_days(@today-(6*@stale)) > last_hit
-      AND innocent_hits + spam_hits < 50;
-COMMIT;
-
--- For users NOT having set 'trainingMode' in their preferences (therefor using default/uid 0)
-START TRANSACTION;
-  DELETE LOW_PRIORITY QUICK
-    FROM t USING dspam_token_data t
-      LEFT JOIN dspam_preferences p
-        ON (t.uid = p.uid)
-    WHERE @trainingmode = 'TUM'
-      AND NOT EXISTS (SELECT uid FROM dspam_preferences WHERE uid = t.uid AND preference = 'trainingMode' LIMIT 1)
-      AND from_days(@today-(6*@stale)) > last_hit
-      AND innocent_hits + spam_hits < 50;
-COMMIT;
-
---
--- Delete signatures older than @stale days
+-- Delete stale signatures
 --
 START TRANSACTION;
-  DELETE LOW_PRIORITY QUICK
-    FROM dspam_signature_data
-    WHERE from_days(@today-(@stale-1)) > created_on;
+DELETE LOW_PRIORITY QUICK
+  FROM dspam_signature_data
+  WHERE from_days(@today-@PurgeSignatures) > created_on;
 COMMIT;
