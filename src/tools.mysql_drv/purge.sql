957d913c2224ab460227a80c2ecdd3d8aa15ef7f
Stevan Bajic <stevan@bajic.ch>
2009-12-19 18:03:07 +0100

    Updated purge.sql SQL script for MySQL
    MFM: 0 days
    Bug-ID: none
    Bug-Reported-By: none
    Security: none

--- src/tools.mysql_drv/purge.sql
+++ src/tools.mysql_drv/purge.sql
@@ -1,37 +1,64 @@
--- $Id: purge.sql,v 1.7 2009/12/18 00:43:54 sbajic Exp $
+-- $Id: purge.sql,v 1.8 2009/12/19 17:50:03 sbajic Exp $
+
+-- ---------------------------------------------------------------------------
+--  ! ! ! ! ! ! ! THIS FILE SHOULD ONLY BE USED ON MYSQL < 4.1 ! ! ! ! ! ! !
+--  ! ! ! ! ! ! ! FOR MYSQL 4.1+ PLEASE USE purge-4.1.sql FILE ! ! ! ! ! ! !
+-- ---------------------------------------------------------------------------
+
+-- ---------------------------------------------------------------------------
+-- Note: Should you have modified your dspam.conf to have other intervals for
+--       the purging then please modify this SQL file to be in sync with your
+--       dspam.conf.
+--
+-- Note: It is difficult to purge neutral tokens with SQL clauses the same
+--       way as dspam_clean is doing it. So you should still run dspam_clean
+--       with the "-u" parameter from time to time.
+-- ---------------------------------------------------------------------------
 
 --
 -- Set some defaults
 --
-SET @today=to_days(current_date());
-SET @stale=15;
+SET @PurgeSignatures = 14;          -- Stale signatures
+SET @PurgeUnused     = 90;          -- Unused tokens
+SET @PurgeHapaxes    = 30;          -- Tokens with less than 5 hits (hapaxes)
+SET @PurgeHits1S     = 15;          -- Tokens with only 1 spam hit
+SET @PurgeHits1I     = 15;          -- Tokens with only 1 innocent hit
+SET @today           = to_days(current_date());
 
 --
--- Delete fairly neutral tokens older than 4*@stale days
+-- Delete tokens with less than 5 hits (hapaxes)
 --
 DELETE
   FROM dspam_token_data
-    WHERE from_days(@today-(4*@stale)) > last_hit
-      AND (innocent_hits*2) + spam_hits < 5;
+    WHERE from_days(@today-@PurgeHapaxes) > last_hit
+      AND (2*innocent_hits)+spam_hits < 5;
 
 --
--- Delete tokens seen only one time per class in the past @stale days
+-- Delete tokens with only 1 spam hit
 --
 DELETE
   FROM dspam_token_data
-    WHERE from_days(@today-@stale) > last_hit
-      AND ((innocent_hits = 1 AND spam_hits = 0) OR (innocent_hits = 0 AND spam_hits = 1));
+    WHERE from_days(@today-@PurgeHits1S) > last_hit
+      AND innocent_hits = 0 AND spam_hits = 1;
 
 --
--- Delete all tokens never seen in the last 4*@stale days
+-- Delete tokens with only 1 innocent hit
 --
 DELETE
   FROM dspam_token_data
-    WHERE from_days(@today-(6*@stale)) > last_hit;
+    WHERE from_days(@today-@PurgeHits1I) > last_hit
+      AND innocent_hits = 1 AND spam_hits = 0;
 
 --
--- Delete signatures older than @stale days
+-- Delete old tokens not seen for a while
+--
+DELETE
+  FROM dspam_token_data
+    WHERE from_days(@today-@PurgeUnused) > last_hit;
+
+--
+-- Delete stale signatures
 --
 DELETE
   FROM dspam_signature_data
-    WHERE from_days(@today-(@stale-1)) > created_on;
+    WHERE from_days(@today-@PurgeSignatures) > created_on;
