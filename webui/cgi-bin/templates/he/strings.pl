14e120d0fcae41d9bb318682ddfcb96e60de4747
Stevan Bajic <stevan@bajic.ch>
2009-12-28 11:02:51 +0100

    Adding strings.pl to Hebrew Web UI (translated by Dudi Goldenberg)
    MFM: 0 days
    Bug-ID: none
    Bug-Reported-By: none
    Security: none

---
+++ webui/cgi-bin/templates/he/strings.pl
@@ -0,0 +1,90 @@
+#!/usr/bin/perl
+
+# $Id: strings.pl,v 1.01 2009/12/24 03:42:33 sbajic Exp $
+# DSPAM
+# COPYRIGHT (C) DSPAM PROJECT 2002-2009
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; version 2
+# of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+$LANG{'lang_name'}			= "עברית";
+$LANG{'lang_select'}			= "שפה";
+
+$LANG{'empty'}				= "ריק";
+$LANG{'admin_suite'}			= "ערכת ניהול";
+$LANG{'alert_name'}			= "שם ההתראה";
+$LANG{'remove_alert'}			= "הסר";
+$LANG{'user_form'}			= "מערכת הגנה סטטיסטית עבור";
+$LANG{'user_form_submit'}		= "החלף";
+
+$LANG{'admin_form'}			= "מערכת הגנה סטטיסטית עבור <b>מנהל המערכת</b>";
+$LANG{'admin_form_submit'}		= "החלף";
+
+$LANG{'option_disable_filtering'}	= "הפסק שימוש ב-DSPAM";
+$LANG{'option_enable_filtering'}	= "אפשר שימוש ב-DSPAM";
+
+$LANG{'quarantine_rating'}		= "ניקוד";
+$LANG{'quarantine_date'}		= "תאריך";
+$LANG{'quarantine_from'}		= "מאת";
+$LANG{'quarantine_subject'}		= "נושא";
+
+$LANG{'history_show'}			= "הצג";
+$LANG{'history_show_all'}		= "הכל";
+$LANG{'history_show_spam'}		= "זבל";
+$LANG{'history_show_innocent'}		= "תקין";
+$LANG{'history_show_whitelisted'}	= "מחוסן";
+$LANG{'history_retrain_as_spam'}	= "זבל";
+$LANG{'history_retrain_as_innocent'}	= "תקין";
+$LANG{'history_retrain_as'}		= "כ-";
+$LANG{'history_retrain_undo'}		= "בטל";
+$LANG{'history_retrained'}		= "נלמדו";
+$LANG{'history_label_resend'}		= "נשלח שוב";
+$LANG{'history_label_whitelist'}	= "מחוסן";
+$LANG{'history_label_spam'}		= "דואר זבל";
+$LANG{'history_label_innocent'}		= "תקין";
+$LANG{'history_label_miss'}		= "החטאה";
+$LANG{'history_label_virus'}		= "וירוס";
+$LANG{'history_label_RBL'}		= "רשימה שחורה";
+$LANG{'history_label_block'}		= "חסום";
+$LANG{'history_label_corpus'}		= "ממאגר";
+$LANG{'history_label_unknown'}		= "לא ידוע";
+$LANG{'history_label_error'}		= "שגיאה";
+
+$LANG{'error_no_historic'}		= "לא נמצא מידע היסטורי.";
+$LANG{'error_cannot_open_log'}		= "לא ניתן לפתוח את קובץ ה-לוג";
+$LANG{'error_no_identity'}		= "שגיאת מערכת. לא ניתן לקבוע את זהותך.";
+$LANG{'error_invalid_command'}		= "פקודה לא חןקית";
+$LANG{'error_cannot_write_prefs'}	= "לא ניתן לכתוב את ההעדפות";
+$LANG{'error_no_sigid'}			= "לא צויין מציין הןדעה )MSG ID(";
+$LANG{'error_no_alert_specified'}	= "לא צויינה התראה.";
+$LANG{'error_message_part1'}		= "השגיאה הבאה ארעה בזמן עיבוד בקשתך:";
+$LANG{'error_message_part2'}		= "אם בעיה לא לא נעלמת, נא פנה למנהל המערכת.";
+$LANG{'error_filesystem_scale'}		= "לא ניתן לקבוע את הגדרת המערכת";
+$LANG{'error_load_default_prefs'}	= "לא ניתן לטעון את העדפות ברירת המחדל";
+$LANG{'error_access_denied'}		= "הגישה נשחתה";
+
+$LANG{'graph_legend_nb_messages'}	= "מספר ההודעות";
+$LANG{'graph_legend_spam'}		= "דואר זבל";
+$LANG{'graph_legend_good'}		= "תקין";
+$LANG{'graph_legend_inoculations'}	= "חיסונים";
+$LANG{'graph_legend_corpusfeds'}	= "ממאגר";
+$LANG{'graph_legend_virus'}		= "וירוס";
+$LANG{'graph_legend_RBL'}		= "ברשימה שיורה";
+$LANG{'graph_legend_blocklisted'}	= "נחסםו";
+$LANG{'graph_legend_whitelisted'}	= "מחוסנים";
+$LANG{'graph_legend_nonspam'}		= "תקין";
+$LANG{'graph_legend_spam_misses'}	= "החטאות דואר זבל";
+$LANG{'graph_legend_falsepositives'}	= "החטאות דואר תקין";
+
+1;
