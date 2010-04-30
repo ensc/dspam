d0be9f348b9d32caaf7b73c1597c6e10a4578c55
Stevan Bajic <stevan@bajic.ch>
2010-04-30 21:05:52 +0200

    Escape SQL query in _pgsql_drv_get_dbversion() for PostgreSQL driver
    MFM: 0 days
    Bug-ID: none
    Bug-Reported-By: Ed Szynaka <szynaka@localnet.com>
    Security: none

--- src/pgsql_drv.c
+++ src/pgsql_drv.c
@@ -1,4 +1,4 @@
-/* $Id: pgsql_drv.c,v 1.737 2010/04/30 18:18:24 sbajic Exp $ */
+/* $Id: pgsql_drv.c,v 1.738 2010/04/30 21:02:31 sbajic Exp $ */
 
 /*
  DSPAM
@@ -3035,7 +3035,7 @@ _pgsql_drv_get_dbversion(struct _pgsql_drv_storage *s, unsigned int range)
   }
 
   /* detect postgres version */
-  snprintf (query, sizeof (query), "SELECT coalesce(substring(split_part(split_part(version(),' ',2),'.',%d) FROM '\\\\d+')::int2,0)", range);
+  snprintf (query, sizeof (query), "SELECT coalesce(substring(split_part(split_part(version(),' ',2),'.',%d) FROM E'\\\\d+')::int2,0)", range);
 
   result = PQexec(s->dbh, query);
   if ( !result || (PQresultStatus(result) != PGRES_TUPLES_OK && PQresultStatus(result) != PGRES_NONFATAL_ERROR) )
