1e556217bfe505b37a0920119f22abf66c6e8c68
Stevan Bajic <stevan@bajic.ch>
2011-05-09 23:05:10 +0200

    Update SQL files for MySQL to allow table creation for MySQL >=5.5.x
    MFM: 1 day
    Bug-ID: 3299508
    Bug-Reported-By: Colin Brace <matamoros@users.sourceforge.net>
    Security: none

--- src/tools.mysql_drv/virtual_user_aliases.sql
+++ src/tools.mysql_drv/virtual_user_aliases.sql
@@ -1,8 +1,8 @@
-# $Id: virtual_user_aliases.sql,v 1.2 2009/05/25 11:52:02 sbajic Exp $
+# $Id: virtual_user_aliases.sql,v 1.3 2011/05/09 23:01:51 sbajic Exp $
 
 create table dspam_virtual_uids (
   uid int unsigned not null,
   username varchar(128) not null
-) type=MyISAM;
+) engine=MyISAM;
 
 create unique index id_virtual_uids_01 on dspam_virtual_uids(username);
--- src/tools.mysql_drv/virtual_users.sql
+++ src/tools.mysql_drv/virtual_users.sql
@@ -1,9 +1,9 @@
-# $Id: virtual_users.sql,v 1.22 2011/01/07 00:59:32 sbajic Exp $
+# $Id: virtual_users.sql,v 1.23 2011/05/09 23:02:12 sbajic Exp $
 
 create table dspam_virtual_uids (
-  uid int unsigned not null primary key AUTO_INCREMENT,
+  uid int unsigned not null primary key auto_increment,
   username varchar(128)
-) type=MyISAM;
+) engine=MyISAM;
 
 create unique index id_virtual_uids_01 on dspam_virtual_uids(username);
 
